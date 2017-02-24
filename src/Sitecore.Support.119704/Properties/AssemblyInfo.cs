c517267e4579f918a2ae3ebefd6013a2b5fa6c97
Marina Kravchenko <mru@sitecore.net>
2017-02-24 18:02:59 +0200

    Initial commit.

---
+++ src/Sitecore.Support.119704/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.119704")]
+[assembly: AssemblyProduct("Sitecore.Support.119704")]
+[assembly: ComVisible(false)]
