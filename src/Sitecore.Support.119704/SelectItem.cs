23c054ca504eb9ef92f55213f908d23b41b03fb5
Dmitry Kolinchuk <dmko@sitecore.net>
2017-03-29 18:26:46 +0300

    Prevents the occurrence of "Do you want to save the changes to the item?" dialog box when adding an item to the existing bucket node

---
+++ src/Sitecore.Support.119704/SelectItem.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Buckets.Managers;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Web.UI.Sheer;
+using System;
+
+namespace Sitecore.Support.Buckets.Pipelines.UI.AddFromTemplate
+{
+    public class SelectItem
+    {
+        public void Execute(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (!args.HasResult)
+            {
+                return;
+            }
+            Item item = (Context.ContentDatabase ?? Context.Database).GetItem(args.Result);
+            if (item != null && BucketManager.IsItemContainedWithinBucket(item))
+            {
+                //Context.ClientPage.ClientResponse.Eval("request.currentCommand = Number.MAX_VALUE;scForm.invoke(\"item:load(id=" + item.ID + ")\")");
+                Context.ClientPage.ClientResponse.Eval("setTimeout(function(){ scForm.invoke(\"item:load(id=" + item.ID + ")\"); }, 0)");
+            }
+        }
+    }
+}
